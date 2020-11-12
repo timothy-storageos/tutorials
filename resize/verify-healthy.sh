9b4479aa92b5ef1c05c4c52765438bc681a8e497
Tim Benger <tim.benger@storageos.com>
2020-11-12 17:05:24 +0000

    reinstate the clear commands now its working

--- resize/verify-healthy.sh
+++ resize/verify-healthy.sh
@@ -7,9 +7,9 @@ for host in ${!hosts[@]}; do
 #Use the nodes endpoint rather than health because it takes longer for nodes
 #endpoint to come up and it seems to more accurately reflect when the cluster is
 #ready
-    # clear;
+    clear;
     while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://${hosts[$host]}:5705 -u storageos:storageos)" != "200" ]]; do
-      # clear
+      clear
       printf 'Waiting for storageOS to initialise on all hosts. This will take around 2 minutes...' 
       sleep 5;
     done
